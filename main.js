2810424974de78c9474513a6f93cb54c4b8f570f
yairadanbonahora <bonahorayair@gmail.com>
2021-12-29 20:53:28 -0300

    Primera entrega

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 alert ("Bienvenido a Metalúrgica Claudio");
 const usuario = prompt ("Por favor cree un usuario");
+console.log ("Usuario: " + usuario)
 const contraseñaIngresada = prompt ("Ingrese una contraseña de 4 dígitos");
     while (contraseñaIngresada.length < 4 || contraseñaIngresada.length > 4) {
         alert ("Contraseña inválida");
         contraseñaIngresada = prompt ("Por favor ingrese una nueva contraseña de 4 dígitos");
     } 
-console.log (contraseñaIngresada);
+console.log ("Contraseña: " + contraseñaIngresada);
 
 do {
     let option = showMenu ();
-    if (option === "ESC") {
-        alert ("Adiós");
-        break; 
-    } else {
-        let quantity = parseInt (prompt ("Indique cantidad"));
-        addToCart (option, quantity);
+    let quantity = parseInt (prompt ("Indique cantidad"));
+        // addToCart (option, quantity);
         resp = prompt ("¿Desea seguir comprando? S/N");
-    }  
 } while (resp === "S"); 
 
-if (cart > 0) {
-    alert (`Su compra tiene un total de $${cart}. Gracias por su compra!`);
-}
 
 alert ("Vuelva pronto!");
